f94db50f286a490290232f01eedd1c40afc1c6b3
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-20 20:11:15 +0100

    Add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Home = () => (
+  <Route path="/">
+    <div>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
+        Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
+        Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea. Veniam laborum dolor, atque sint nostrum ea sed soluta corrupti mollitia enim commodi culpa impedit incidunt eligendi illum! Molestias?
+        Neque quasi explicabo, sequi eaque sapiente laboriosam voluptates eos reiciendis, quos magni et recusandae animi libero cum error quibusdam, consequuntur molestias soluta facere accusantium! Necessitatibus excepturi nostrum delectus a minus.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
+        Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
+        Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea. Veniam laborum dolor, atque sint nostrum ea sed soluta corrupti mollitia enim commodi culpa impedit incidunt eligendi illum! Molestias?
+        Neque quasi explicabo, sequi eaque sapiente laboriosam voluptates eos reiciendis, quos magni et recusandae animi libero cum error quibusdam, consequuntur molestias soluta facere accusantium! Necessitatibus excepturi nostrum delectus a minus.
+      </p>
+    </div>
+  </Route>
+);
+export default Home;
