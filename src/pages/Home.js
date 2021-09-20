adc2c9bc748444689d444ea511a04ba943890f98
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-20 21:13:05 +0100

    Add styling to Home, Calculator, and Quote pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,19 @@ import { Route } from 'react-router-dom';
 
 const Home = () => (
   <Route path="/">
-    <div>
-      <h1>Welcome to our page!</h1>
-      <p>
+    <div className="container">
+      <h1 style={{ marginBottom: '40px' }}>Welcome to our page!</h1>
+      <p style={{ marginBottom: '30px' }}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
         Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
         Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
-        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea. Veniam laborum dolor, atque sint nostrum ea sed soluta corrupti mollitia enim commodi culpa impedit incidunt eligendi illum! Molestias?
-        Neque quasi explicabo, sequi eaque sapiente laboriosam voluptates eos reiciendis, quos magni et recusandae animi libero cum error quibusdam, consequuntur molestias soluta facere accusantium! Necessitatibus excepturi nostrum delectus a minus.
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea.
       </p>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
         Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
         Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
-        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea. Veniam laborum dolor, atque sint nostrum ea sed soluta corrupti mollitia enim commodi culpa impedit incidunt eligendi illum! Molestias?
-        Neque quasi explicabo, sequi eaque sapiente laboriosam voluptates eos reiciendis, quos magni et recusandae animi libero cum error quibusdam, consequuntur molestias soluta facere accusantium! Necessitatibus excepturi nostrum delectus a minus.
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea.
       </p>
     </div>
   </Route>
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,19 +2,19 @@
 import React from 'react';
 
 const Quote = () => (
-  <div>
-    <p>
+  <div className="container">
+    <h3>
       Mathematics as an expression of the human mind reflects the active will, the contemplative reason, and the desire for aesthetic perfection. Its basic elements are logic and intuition, analysis and construction, generality and individuality.
       — Richard Courant, German-American mathematician
-    </p>
-    <p>
+    </h3>
+    <h3>
       As far as the laws of mathematics refer to reality, they are not certain, and as far as they are certain, they do not refer to reality.
       — Albert Einstein
-    </p>
-    <p>
+    </h3>
+    <h3>
       What is mathematics? It is only a systematic effort of solving puzzles posed by nature.
       — Shakuntala Devi
-    </p>
+    </h3>
   </div>
 );
 
