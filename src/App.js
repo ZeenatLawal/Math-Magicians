06bd2e453634f9b4a2d0a2e96f171e1a249ae5f6
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-20 21:15:24 +0100

    Create routes using react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NotMatch from './pages/NotMatch';
 
 function App() {
   return (
-    <div className="calcDiv"><Calculator /></div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/calculator">
+          <Calculator />
+        </Route>
+        <Route path="/quote">
+          <Quote />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </>
   );
 }
 export default App;
